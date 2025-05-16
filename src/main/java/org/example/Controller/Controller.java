ac0a376c557bc9474c83612700db0c90c323dd35
J-uds <206530139+J-uds@users.noreply.github.com>
2025-05-16 11:20:07 +0200

    Edited startGame method (playerMove)

--- src/main/java/org/example/Controller/Controller.java
+++ src/main/java/org/example/Controller/Controller.java
@@ -26,5 +26,13 @@ public class Controller {
     public void startGame() {
         view.showMessage("Juguemos tic tac toe\nEl tablero se compone de fila 0 - 1 y 2 y de columna 0 - 1 y 2");
         view.displayBoard(board);
+        int[] playerMove = view.askForMove(currentPlayer);
+        int row = playerMove[0];
+        int column = playerMove[1];
+        if (!board.isCellEmpty(row, column)){
+            view.showMessage("La posición está ocupada");
+        }
     }
+
+
 }
