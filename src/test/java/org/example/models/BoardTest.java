f885429414bd7628cc24b72b926726ea517f2888
Alexandracoder <acasaviajes@gmail.com>
2025-05-19 16:23:30 +0200

    Added changes Readme, Test board  change O cirilic to latin O

--- src/test/java/org/example/models/BoardTest.java
+++ src/test/java/org/example/models/BoardTest.java
@@ -7,20 +7,17 @@ import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
 class BoardTest {
-Board board;
+    Board board;
 
     @BeforeEach
     void setUp() {
-       board = new Board();
+        board = new Board();
     }
 
     @AfterEach
     void tearDown() {
+        // Aquí puedes limpiar recursos si necesitas, pero no es obligatorio
     }
-
-   /* @Test
-    void initializeBoard() {
-    } */
 
     @Test
     void isCellEmpty() {
@@ -33,7 +30,10 @@ Board board;
     }
 
     @Test
-    void placeMove() {
+    void placeMoveShouldUpdateBoard() {
+        board.placeMove(1, 2, 'X'); // Colocar 'X' en fila 1, columna 2
+        char[][] actualBoard = board.getBoard();
+        assertEquals('X', actualBoard[1][2]); // Verificar que en esa posición está 'X'
     }
 
     @Test
@@ -43,14 +43,27 @@ Board board;
         board.placeMove(0, 2, 'X');
         assertEquals('X', board.checkWinner());
 
-        board.placeMove(0, 0, 'О');
-        board.placeMove(0, 1, 'О');
-        board.placeMove(0, 2, 'О');
-        assertEquals('О', board.checkWinner());
+        board = new Board();
 
+        board.placeMove(0, 0, 'O');
+        board.placeMove(0, 1, 'O');
+        board.placeMove(0, 2, 'O');
+        assertEquals('O', board.checkWinner());
     }
 
     @Test
     void getBoard() {
+        char[][] actualBoard = board.getBoard();
+        assertNotNull(actualBoard);
+        assertEquals(3, actualBoard.length);
+        assertEquals(3, actualBoard[0].length);
+
+        for (int row = 0; row < actualBoard.length; row++) {
+            for (int col = 0; col < actualBoard[row].length; col++) {
+                assertEquals('_', actualBoard[row][col]);
+            }
+        }
     }
 }
+
+
