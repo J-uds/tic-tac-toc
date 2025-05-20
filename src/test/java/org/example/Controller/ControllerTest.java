8df3bfc684974e44362e8163e2ed0b31a3862e87
NepyAnna <nepyann@gmail.com>
2025-05-20 12:12:45 +0200

    Addded startGame test

---
+++ src/test/java/org/example/Controller/ControllerTest.java
@@ -0,0 +1,46 @@
+package org.example.Controller;
+
+import org.example.models.Board;
+import org.example.view.View;
+import org.junit.jupiter.api.Test;
+
+import java.io.*;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class ControllerTest {
+
+    @Test
+    void startGame() {
+        String simulatedInput = String.join(System.lineSeparator(),
+                "0 0",
+                "1 1",
+                "0 1",
+                "1 2",
+                "0 2");
+
+        InputStream originalIn = System.in;
+        PrintStream originalOut = System.out;
+
+        ByteArrayInputStream simulatedIn = new ByteArrayInputStream(simulatedInput.getBytes());
+        ByteArrayOutputStream simulatedOut = new ByteArrayOutputStream();
+
+        try {
+            System.setIn(simulatedIn);
+            System.setOut(new PrintStream(simulatedOut));
+
+            Board board = new Board();
+            View view = new View();
+            Controller controller = new Controller();
+            controller.startGame();
+
+            String output = simulatedOut.toString();
+
+            assertTrue(output.contains("¡Ganador: X!"));
+
+        } finally {
+            System.setIn(originalIn);
+            System.setOut(originalOut);
+        }
+    }
+}
